a9d7947d0a3b776915e4afc164df76be7741ff25
Tricia Swift <tricia.swift8@gmail.com>
2024-02-28 16:02:31 -0600

    GET endpoints created

--- Models/DTOs/ServiceTicketDTO.cs
+++ Models/DTOs/ServiceTicketDTO.cs
@@ -4,8 +4,10 @@ public class ServiceTicketDTO
 {
     public int Id { get; set;}
     public int CustomerId { get; set;}
-    public int EmployeeId { get; set;}
-    public string? Description { get; set;}
+    public int? EmployeeId { get; set;}
+    public string Description { get; set;}
     public bool Emergency { get; set;}
     public DateTime DateCompleted { get; set;}
+    public EmployeeDTO Employee { get; set; }
+    public CustomerDTO Customer { get; set; }
 }
--- Program.cs
+++ Program.cs
@@ -67,15 +67,33 @@ app.MapGet("/servicetickets", () =>
 app.MapGet("/servicetickets/{id}", (int id) =>
 {
     ServiceTicket serviceTicket = serviceTickets.FirstOrDefault(st => st.Id == id);
+    
     if (serviceTicket == null)
     {
         return Results.NotFound();
     }
+    
+    Employee employee = employees.FirstOrDefault(e => e.Id == serviceTicket.EmployeeId);
+
+    Customer customer = customers.FirstOrDefault(e => e.Id == serviceTicket.CustomerId);
+    
     return Results.Ok(new ServiceTicketDTO
     {
         Id = serviceTicket.Id,
         CustomerId = serviceTicket.CustomerId,
         EmployeeId = serviceTicket.EmployeeId,
+        Employee = employee == null ? null : new EmployeeDTO
+        {
+            Id = employee.Id,
+            Name = employee.Name,
+            Specialty = employee.Specialty
+        },
+        Customer = customer == null ? null : new CustomerDTO
+        {
+            Id = customer.Id,
+            Name = customer.Name,
+            Address = customer.Address
+        },
         Description = serviceTicket.Description,
         Emergency = serviceTicket.Emergency,
         DateCompleted = serviceTicket.DateCompleted
@@ -101,11 +119,22 @@ app.MapGet("/customers/{id}", (int id) =>
     {
         return Results.NotFound();
     }
+
+    List<ServiceTicket> tickets = serviceTickets.Where(st => st.CustomerId == id).ToList();
     return Results.Ok(new CustomerDTO
     {
         Id = customer.Id,
         Name = customer.Name,
-        Address = customer.Address
+        Address = customer.Address,
+        ServiceTickets = tickets.Select(t => new ServiceTicketDTO
+        {
+            Id = t.Id,
+            CustomerId = t.CustomerId,
+            EmployeeId = t.EmployeeId,
+            Description = t.Description,
+            Emergency = t.Emergency,
+            DateCompleted = t.DateCompleted
+        }).ToList()
     });
 });
 
@@ -128,11 +157,21 @@ app.MapGet("/employees/{id}", (int id) =>
     {
         return Results.NotFound();
     }
+    List<ServiceTicket> tickets = serviceTickets.Where(st => st.EmployeeId == id).ToList();
     return Results.Ok(new EmployeeDTO
     {
         Id = employee.Id,
         Name = employee.Name,
-        Specialty = employee.Specialty
+        Specialty = employee.Specialty,
+        ServiceTickets = tickets.Select(t => new ServiceTicketDTO
+        {
+            Id = t.Id,
+            CustomerId = t.CustomerId,
+            EmployeeId = t.EmployeeId,
+            Description = t.Description,
+            Emergency = t.Emergency,
+            DateCompleted = t.DateCompleted
+        }).ToList()
     });
 });
 
